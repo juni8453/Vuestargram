8bc5fa41332af4a5cfe1955fa89d13dfda585221
ByeoungJoon Jeon <juni8453@naver.com>
2023-12-23 15:44:28 +0900

    레이아웃 설정 및 더미데이터 바인딩

---
+++ src/components/Container.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div v-for="(post, i) in posts" :key="i">
+      <Post :post="post"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Post from './Post.vue'
+
+export default {
+  name: "Container",
+  components: {
+    Post : Post,
+  },
+  props: {
+    posts: Array
+  },
+}
+</script>
+
+<style>
+
+</style>
