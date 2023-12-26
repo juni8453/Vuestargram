96945e6933902e8782dccee4f12859f847cb1d85
ByeoungJoon Jeon <juni8453@naver.com>
2023-12-26 15:03:16 +0900

    vuex4 설치 및 셋팅
    - state 수정 시 컴포넌트에서 수정하면 안된다.

--- src/main.js
+++ src/main.js
@@ -3,10 +3,11 @@ import App from './App.vue'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Router from './router/Router.js'
+import Store from './json/store.js'
 import mitt from 'mitt'
 
 let emitter = mitt();
 let app = createApp(App);
 app.config.globalProperties.emitter = emitter;
 
-app.use(Router).mount('#app')
+app.use(Router).use(Store).mount('#app')
